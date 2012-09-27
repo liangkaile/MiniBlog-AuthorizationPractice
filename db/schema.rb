d4b3c4807d90deb0db4969a8216075ec0c5ed322
liangkaile <liangkaile@hotmail.com>
2012-09-26 23:28:49 -0600

    sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906213739) do
+ActiveRecord::Schema.define(:version => 20120927043513) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(:version => 20120906213739) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
